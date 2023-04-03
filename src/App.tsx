01186551086dc6709012ba040a16ebc4cc93159f
Naël Achkar <naelachkar@hotmail.fr>
2023-04-04 00:36:11 +0300

    Initialising translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useTranslation } from "react-i18next";
 import "./App.css";
 
 export default function App() {
+  const { t } = useTranslation();
+
   return (
     <div className="App">
-      <h1>Vite + React</h1>
+      <h1>{t("hello")}</h1>
+      <h1>
+        {t("my name is")}{" "}
+        <span className="h1-gradient">{t("Naël Achkar")}</span>.
+      </h1>
     </div>
   );
 }
